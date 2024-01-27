0bb52d24bdbfd6fdbb0e16315066f850415e577f
Ayoub <124147277+aezzahir@users.noreply.github.com>
2024-01-27 11:47:51 +0000

    Archiving

---
+++ 1-pack_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+1. Compress before sending
+"""
+from fabric.api import *
+from datetime import datetime
+
+
+def do_pack():
+    """
+    must return the archive path if the archive has been
+    correctly generated. Otherwise, it should return None
+    """
+    now = datetime.now()
+    time_string = now.strftime("%Y%m%d%H%M%S")
+    archive_name = "web_static_{}.tgz".format(time_string)
+    local("mkdir -p versions")
+    archive_path = "versions/{}".format(archive_name)
+    command = "tar -czvf {} web_static".format(archive_path)
+    result = local(command)
+    if result.failed:
+        return None
+    return archive_path
