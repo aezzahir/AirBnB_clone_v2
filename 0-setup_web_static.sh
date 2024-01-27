7c2bfbe353cb29ac7fda8e878cb1fd05b2eb41b9
Ayoub <124147277+aezzahir@users.noreply.github.com>
2024-01-27 11:17:10 +0000

    sudo mkdir -p /data/web_static/releases/test/

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# Install Nginx if not already installed
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTP'
+# Create necessary folders if they don't exist
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+# Create a fake HTML file with content
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
