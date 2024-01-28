6146b1d0d66824357cfdfe1fb99a7eb69f5a1114
Ayoub <124147277+aezzahir@users.noreply.github.com>
2024-01-28 17:23:32 +0000

    4. Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application:
+    Your web application must be listening on 0.0.0.0, port 5000
+    Routes:
+        /: display “Hello HBNB!”
+        /hbnb: display “HBNB”
+    You must use the option strict_slashes=False in your route definition
+"""
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """
+    Display Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """
+    Display HBNB
+    """
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_route(text):
+    """
+    Display C is ...
+    """
+    return f'C {escape(text).replace("_", " ")}'
+
+
+@app.route("/python/", defaults={'text': "is_cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    """
+    Display Python is ...
+    """
+    return f"Python {escape(text)}".replace("_", " ")
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def number(n):
+    """
+    Is a number ...
+    """
+    if (int(n) == float(n)):
+        return f"{escape(n)} is a number"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
