298b10144fe0dfc855da67caca7a64c41d5e1682
Ayoub <124147277+aezzahir@users.noreply.github.com>
2024-01-28 19:08:56 +0000

    7. Improve engines

--- models/state.py
+++ models/state.py
@@ -3,8 +3,25 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+import models
+from models.city import City
+import os
+from os import environ
+
 
 class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    if environ.get('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        @property
+        def cities(self):
+            "Return list of City objects"
+            dict_all_cities = models.storage.all(City)
+            list_cities = []
+            for city in dict_all_cities.values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
